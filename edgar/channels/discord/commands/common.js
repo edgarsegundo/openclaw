159d8f6375428d50d3b8f33a64049baf7b673e5f
edgarsegundo <edgar.segundo@gmail.com>
2026-04-14 19:16:36 -0300

    updates

--- edgar/channels/discord/commands/common.js
+++ edgar/channels/discord/commands/common.js
@@ -7,7 +7,7 @@ export function createTempInputFile(itemIndex, action = "approve") {
   const inputObj = {
     rss_fetcher_output_artifact_file_name_pattern: "artifacts/rss-fetcher/rss-artifact-visto-americano-{date}.json",
     blog_context: "Blog voltado para brasileiros interessados em assuntos ligados ao visto americano e aos serviços consulares dos EUA. O público busca informações práticas, atualizadas e confiáveis sobre solicitação, renovação, entrevistas, documentação, mudanças nas regras, notícias do consulado e outros temas relacionados ao processo consular.",
-    min_items: 3,
+    min_items: 20,
     min_score: 7,
     action, // "approve" ou "del"
     item_index: itemIndex
